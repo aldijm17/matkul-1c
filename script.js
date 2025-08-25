bcc6db2a1964ff730add59fd3cdded3d3867ee3d
Aldi Julse Makruf <158381668+aldijm17@users.noreply.github.com>
2025-08-25 20:37:47 +0700

    new

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 import { db } from "./firebase.js";
 import { collection, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
@@ -6,14 +7,17 @@ const jadwalList = document.getElementById("jadwalList");
 async function loadJadwal() {
   jadwalList.innerHTML = "";
   const querySnapshot = await getDocs(collection(db, "jadwal"));
+  
   querySnapshot.forEach((docSnap) => {
     const data = docSnap.data();
     jadwalList.innerHTML += `
       <tr>
         <td>${data.hari}</td>
         <td>${data.matkul}</td>
-        <td>${data.jam}</td>
+        <td>${data.waktu_masuk || '-'}</td>
+        <td>${data.waktu_keluar || '-'}</td>
         <td>${data.dosen}</td>
+        <td>${data.ruang || '-'}</td>
         <td>
           <a href="add.html?id=${docSnap.id}">✏️ Edit</a>
           <button onclick="hapus('${docSnap.id}')">🗑️ Hapus</button>
@@ -24,8 +28,10 @@ async function loadJadwal() {
 }
 
 window.hapus = async function(id) {
-  await deleteDoc(doc(db, "jadwal", id));
-  loadJadwal();
+  if (confirm("Apakah Anda yakin ingin menghapus jadwal ini?")) {
+    await deleteDoc(doc(db, "jadwal", id));
+    loadJadwal();
+  }
 }
 
 loadJadwal();
